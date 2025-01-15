17f682022e32048bc5de9b847d9dcc6f691c9495
CodingWonders <101426328+CodingWonders@users.noreply.github.com>
2025-01-15 18:12:20 +0100

    Remove NETSDK1129 error from publish script

--- publish.bat
+++ publish.bat
@@ -16,9 +16,9 @@ cls
 echo Publishing self-contained binaries...
 
 :: Publish self-contained versions
-dotnet publish -r:win-x86 --sc -o "bin\release\net9.0\sc\win-x86"
-dotnet publish -r:win-x64 --sc -o "bin\release\net9.0\sc\win-x64"
-dotnet publish -r:win-arm64 --sc -o "bin\release\net9.0\sc\win-arm64"
+dotnet publish -r:win-x86 --sc -o "bin\release\net9.0\sc\win-x86" --framework net9.0
+dotnet publish -r:win-x64 --sc -o "bin\release\net9.0\sc\win-x64" --framework net9.0
+dotnet publish -r:win-arm64 --sc -o "bin\release\net9.0\sc\win-arm64" --framework net9.0
 
 if exist "bin\release\net9.0\sc" (
 	echo Zipping self-contained binaries...
@@ -29,11 +29,11 @@ if exist "bin\release\net9.0\sc" (
 echo Publishing regular binaries...
 
 :: Publish release for x86, amd64 and arm64
-dotnet publish -r:win-x86
+dotnet publish -r:win-x86 --framework net9.0
 rd "bin\release\net9.0\win-x86\publish" /s /q
-dotnet publish -r:win-x64
+dotnet publish -r:win-x64 --framework net9.0
 rd "bin\release\net9.0\win-x64\publish" /s /q
-dotnet publish -r:win-arm64
+dotnet publish -r:win-arm64 --framework net9.0
 rd "bin\release\net9.0\win-arm64\publish" /s /q
 
 echo Zipping regular binaries...
